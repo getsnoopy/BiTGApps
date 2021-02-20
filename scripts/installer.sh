5f8a9c859a99cc4deb4564c903e5652a8c7abf09
TheHitMan7 <krtik.vrma@gmail.com>
2021-02-20 14:46:40 +0530

    Improve SPL/USF patch detection
    Signed-off-by: TheHitMan7 <krtik.vrma@gmail.com>

--- scripts/installer.sh
+++ scripts/installer.sh
@@ -939,11 +939,16 @@ boot_A() {
           chmod 0644 $SYSTEM/etc/init/init.boot.rc
           chcon -h u:object_r:system_file:s0 "$SYSTEM/etc/init/init.boot.rc"
         else
+          if [ -n "$(cat $INIT | grep init.spl.rc)" ] && [ -n "$(cat $INIT | grep init.usf.rc)" ]; then
+            insert_line $INIT "import /system/etc/init/init.boot.rc" after 'import /system/etc/init/init.usf.rc' "import /system/etc/init/init.boot.rc"
+          fi
           if [ -n "$(cat $INIT | grep init.spl.rc)" ]; then
             insert_line $INIT "import /system/etc/init/init.boot.rc" after 'import /system/etc/init/init.spl.rc' "import /system/etc/init/init.boot.rc"
-          elif [ -n "$(cat $INIT | grep init.usf.rc)" ]; then
-            insert_line $INIT "import /system/etc/init/init.boot.rc" before 'import /system/etc/init/init.usf.rc' "import /system/etc/init/init.boot.rc"
-          else
+          fi
+          if [ -n "$(cat $INIT | grep init.usf.rc)" ]; then
+            insert_line $INIT "import /system/etc/init/init.boot.rc" after 'import /system/etc/init/init.usf.rc' "import /system/etc/init/init.boot.rc"
+          fi
+          if [ ! -n "$(cat $INIT | grep init.spl.rc)" ] && [ ! -n "$(cat $INIT | grep init.usf.rc)" ]; then
             insert_line $INIT "import /system/etc/init/init.boot.rc" before 'service bootanim /system/bin/bootanimation' "import /system/etc/init/init.boot.rc"
             sed -i '/init.boot.rc/G' $INIT
           fi
